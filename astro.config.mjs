98c804348057a1ef77b80bf101724a2b7e7aaa11
Ben <bzando@mit.edu>
2025-09-22 14:59:17 -0400

    updated paper layout and base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bengineerz.github.io',
-  base: '/academic-website',
   output: 'static',
   integrations: [mdx({ remarkPlugins:[remarkMath], rehypePlugins:[rehypeKatex] }), react(), sitemap()],
   devToolbar: {
